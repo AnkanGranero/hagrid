3726891777896d45ac317a2fac37477b3ae93d86
Andreas Granér <Andreas@graner.me>
2024-08-26 14:34:51 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import CTv from './components/CTv.vue';
+</script>
+
+<template>
+  <CTv />
+</template>
+
+<style lang="scss" >
+body {
+  -webkit-user-select: none;
+  height: 100%;
+  width: 100%;
+  min-height: 100%;
+  margin: 0;
+  min-width: fit-content;
+  min-height: fit-content;
+  background: $hagrid-green;
+
+  margin: 0;
+
+  font-size: 1.5rem;
+  letter-spacing: -1px;
+  font-family: "Open Sans", sans-serif;
+}
+
+</style>
---
+++ src/components/CTv.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+
+defineProps({
+  theme: String,
+})
+
+
+
+
+</script>
+
+<template>
+  <div class="tv-wrapper"
+  >
+</div>
+  
+</template>
+
+<style lang="scss" scoped>
+.tv-wrapper {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  @media only screen and (min-width: $tablet) {
+    position: unset;
+    height: 85%;
+    width: unset;
+    margin: 0 14%;
+  }
+
+}
+
+</style>
