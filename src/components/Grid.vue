ae654bab7497400e832f2545167ba483ee536332
Andreas Granér <Andreas@graner.me>
2024-09-12 16:21:07 +0200

    fix small typo

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -9,28 +9,28 @@ const highlightX = ref(1);
 const highlightY = ref(1);
 
 function colorCalcDif(n, highlightedCoordinate) {
-      
-      let availableDistance = gridSize.value -1; 
 
-      let numbers = [n, highlightedCoordinate];
-      numbers.sort((a, b) => b - a);
-      let high = numbers[0];
-      let low = numbers[1];
-      let difference = high - low;
-      let percentageAway = difference / availableDistance;
+  let availableDistance = gridSize.value - 1;
 
-      let invert = 250 * percentageAway;
-      return 250 - invert;
-    };
+  let numbers = [n, highlightedCoordinate];
+  numbers.sort((a, b) => b - a);
+  let high = numbers[0];
+  let low = numbers[1];
+  let difference = high - low;
+  let percentageAway = difference / availableDistance;
+
+  let invert = 250 * percentageAway;
+  return 250 - invert;
+};
 
 function gridColor(item) {
-  
-  const {x,y} = item;
-  return `background: rgb( ${colorCalcDif(x, highlightX.value)}, ${ colorCalcDif(y, highlightY.value )}, 250)`
-  
+
+  const { x, y } = item;
+  return `background: rgb( ${colorCalcDif(x, highlightX.value)}, ${colorCalcDif(y, highlightY.value)}, 250)`
+
 }
 
-const gridStyling = computed( () => {
+const gridStyling = computed(() => {
   return `grid-template-columns: repeat(${gridSize.value}, 1fr [col-start]);
   grid-template-rows: repeat(${gridSize.value}, 1fr);
 `
@@ -42,49 +42,50 @@ function generateGridClass(item) {
 function pos(item) {
   highlightX.value = item.x;
   highlightY.value = item.y;
-  
- onMounted(() => {
-  
+}
+onMounted(() => {
+
   let y = 1;
 
-  for (let x= 1 ; x <= gridSize.value && y <= gridSize.value; x++ ) {
+  for (let x = 1; x <= gridSize.value && y <= gridSize.value; x++) {
 
-    itemsArray.value.push({x, y})
-    
-    if( x == gridSize.value) {
+    itemsArray.value.push({ x, y })
+
+    if (x == gridSize.value) {
       x = 0;
       y += 1;
     }
 
 
-  }}) 
+  }
+})
 
 </script>
 
 <template>
-   <div class="grid-container" :style="gridStyling">
-                <div v-for="item in itemsArray" @mouseenter="pos(item)" :class="generateGridClass(item)" :style="gridColor(item)">           
-            </div>
+  <div class="grid-container" :style="gridStyling">
+    <div v-for="item in itemsArray" @mouseenter="pos(item)" :class="generateGridClass(item)" :style="gridColor(item)">
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .grid-container {
   display: grid;
-    height: 50vw;
-    width: 50vw;
+  height: 50vw;
+  width: 50vw;
 }
+
 .grid-wrapper {
-    height: 120vh;
+  height: 120vh;
   width: 80vh;
   display: grid;
   grid-auto-rows: auto;
   height: 75%;
   width: 100%;
+
   @media only screen and (min-width: 768px) {
     height: 100%;
   }
 }
-
-
 </style>
